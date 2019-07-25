52392ffd9482a9559a2907fd6d9791d37c2df02f
Viktor Voronin <viktor_voronin@epam.com>
2019-07-25 14:10:55 +0000

    Revert "[EPMDEDP-3421] - Fix job+provisioning bug"
    This reverts commit 30b7487bbbce0b2bb522ea6287ed9e350c2ec917.
    
    Change-Id: I938c3df4795f1d3d6464c38012dd70ac33c69255

--- pkg/jenkins/jenkins.go
+++ pkg/jenkins/jenkins.go
@@ -28,22 +28,7 @@ func Init(url string, username string, token string) (*Client, error) {
 }
 
 func (client Client) TriggerJobProvisioning(codebaseName string, buildTool string) error {
-	isQueued, err := client.IsJobQueued("Job-provisioning")
-	if err != nil {
-		return err
-	}
-	if *isQueued {
-		return errors.New("Job-provisioning already queued")
-	}
-	isRunning, err := client.IsJobRunning("Job-provisioning")
-	if err != nil {
-		return err
-	}
-	if *isRunning {
-		return errors.New("Job-provisioning already running")
-	}
-
-	_, err = client.jenkins.BuildJob("Job-provisioning", map[string]string{
+	_, err := client.jenkins.BuildJob("Job-provisioning", map[string]string{
 		"PARAM":      "true",
 		"NAME":       codebaseName,
 		"BUILD_TOOL": strings.ToLower(buildTool),
