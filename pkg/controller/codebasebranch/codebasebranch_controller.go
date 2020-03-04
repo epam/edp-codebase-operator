33c2b54d5839a34a8ad9b963eae58dda9c7769a7
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2020-03-04 14:56:40 +0200

    [EPMDEDP-4742] Fix codebase branch provisioning
    Change-Id: I3dc0720ebac440d9f94b90c7a0ef9395c2a7173b

--- pkg/controller/codebasebranch/codebasebranch_controller.go
+++ pkg/controller/codebasebranch/codebasebranch_controller.go
@@ -4,26 +4,25 @@ import (
 	"context"
 	"fmt"
 	edpv1alpha1 "github.com/epmd-edp/codebase-operator/v2/pkg/apis/edp/v1alpha1"
-	"github.com/epmd-edp/codebase-operator/v2/pkg/controller/codebasebranch/impl"
+	"github.com/epmd-edp/codebase-operator/v2/pkg/controller/codebasebranch/service"
+	"github.com/epmd-edp/codebase-operator/v2/pkg/openshift"
 	"github.com/epmd-edp/jenkins-operator/v2/pkg/apis/v2/v1alpha1"
 	"github.com/pkg/errors"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"reflect"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 	"time"
 )
-
-/**
-* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
-* business logic.  Delete these comments after modifying this file.*
- */
 
 var log = logf.Log.WithName("codebase-branch-controller")
 
@@ -33,27 +32,41 @@ type CodebaseBranchService interface {
 	Delete(cr *edpv1alpha1.CodebaseBranch)
 }
 
-// Add creates a new CodebaseBranch Controller and adds it to the Manager. The Manager will set fields on the Controller
-// and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
 	return add(mgr, newReconciler(mgr))
 }
 
-// newReconciler returns a new reconcile.Reconciler
 func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	return &ReconcileCodebaseBranch{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+	c := mgr.GetClient()
+	oc := openshift.CreateOpenshiftClients()
+	oc.Client = c
+	return &ReconcileCodebaseBranch{
+		client: mgr.GetClient(),
+		scheme: mgr.GetScheme(),
+		codebaseBranchService: service.CodebaseBranchService{
+			Cs: *oc,
+		},
+	}
 }
 
-// add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
-	// Create a new controller
 	c, err := controller.New("codebasebranch-controller", mgr, controller.Options{Reconciler: r})
 	if err != nil {
 		return err
 	}
 
-	// Watch for changes to primary resource CodebaseBranch
-	err = c.Watch(&source.Kind{Type: &edpv1alpha1.CodebaseBranch{}}, &handler.EnqueueRequestForObject{})
+	pred := predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			oo := e.ObjectOld.(*edpv1alpha1.CodebaseBranch)
+			no := e.ObjectNew.(*edpv1alpha1.CodebaseBranch)
+			if !reflect.DeepEqual(oo.Spec, no.Spec) {
+				return true
+			}
+			return false
+		},
+	}
+
+	err = c.Watch(&source.Kind{Type: &edpv1alpha1.CodebaseBranch{}}, &handler.EnqueueRequestForObject{}, pred)
 	if err != nil {
 		return err
 	}
@@ -63,69 +76,52 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 
 var _ reconcile.Reconciler = &ReconcileCodebaseBranch{}
 
-// ReconcileCodebaseBranch reconciles a CodebaseBranch object
 type ReconcileCodebaseBranch struct {
-	// This client, initialized using mgr.Client() above, is a split client
-	// that reads objects from the cache and writes to the apiserver
-	client client.Client
-	scheme *runtime.Scheme
+	client                client.Client
+	scheme                *runtime.Scheme
+	codebaseBranchService service.CodebaseBranchService
 }
 
-// Reconcile reads that state of the cluster for a CodebaseBranch object and makes changes based on the state read
-// and what is in the CodebaseBranch.Spec
-// Note:
-// The Controller will requeue the Request to be processed again if the returned error is non-nil or
-// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
 func (r *ReconcileCodebaseBranch) Reconcile(request reconcile.Request) (reconcile.Result, error) {
-	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
-	reqLogger.Info("Reconciling CodebaseBranch")
+	rl := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	rl.Info("Reconciling CodebaseBranch")
 
-	// Fetch the CodebaseBranch instance
-	instance := &edpv1alpha1.CodebaseBranch{}
-	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
-	if err != nil {
+	i := &edpv1alpha1.CodebaseBranch{}
+	if err := r.client.Get(context.TODO(), request.NamespacedName, i); err != nil {
 		if k8serrors.IsNotFound(err) {
-			// Request object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-			// Return and don't requeue
 			return reconcile.Result{}, nil
 		}
-		// Error reading the object - requeue the request.
 		return reconcile.Result{}, err
 	}
-	defer r.updateStatus(instance)
 
-	app, err := r.getApplicationByBranch(*instance)
-
+	c, err := r.getCodebaseByBranch(*i)
 	if err != nil {
 		return reconcile.Result{RequeueAfter: 5 * time.Second},
 			errors.Wrapf(err, "couldn't get codebase for branch %s", request.Name)
 	}
 
-	jfn := fmt.Sprintf("%v-%v", app.Name, "codebase")
-	jf, err := r.getJenkinsFolder(jfn, app.Namespace)
+	jfn := fmt.Sprintf("%v-%v", c.Name, "codebase")
+	jf, err := r.getJenkinsFolder(jfn, c.Namespace)
 	if err != nil {
 		return reconcile.Result{RequeueAfter: 5 * time.Second}, err
 	}
 
 	a := jf == nil || !jf.Status.Available
-	if !app.Status.Available && a {
-		log.Info("can't start reconcile for branch", "codebase", app.Name,
-			"codebase status", app.Status.Available, "branch", request.Name, "jenkins folder", a)
+	if !c.Status.Available && a {
+		log.Info("can't start reconcile for branch", "codebase", c.Name,
+			"codebase status", c.Status.Available, "branch", request.Name, "jenkins folder", a)
 		return reconcile.Result{RequeueAfter: 5 * time.Second}, nil
 	}
 
-	codebaseBranch, err := getCodebaseBranchService(r)
-	if err != nil {
-		log.Error(err, "couldn't initializate codebase branch service")
+	if err := r.codebaseBranchService.TriggerReleaseJob(i); err != nil {
+		return reconcile.Result{}, err
 	}
-	codebaseBranch.Create(instance)
 
-	reqLogger.Info("Reconciling CodebaseBranch has been finished")
+	rl.Info("Reconciling CodebaseBranch has been finished")
 	return reconcile.Result{}, nil
 }
 
-func (r *ReconcileCodebaseBranch) getApplicationByBranch(branch edpv1alpha1.CodebaseBranch) (*edpv1alpha1.Codebase, error) {
+func (r *ReconcileCodebaseBranch) getCodebaseByBranch(branch edpv1alpha1.CodebaseBranch) (*edpv1alpha1.Codebase, error) {
 	instance := &edpv1alpha1.Codebase{}
 	err := r.client.Get(context.TODO(), types.NamespacedName{
 		Namespace: branch.Namespace,
@@ -137,19 +133,6 @@ func (r *ReconcileCodebaseBranch) getApplicationByBranch(branch edpv1alpha1.Code
 	}
 
 	return instance, nil
-}
-
-func getCodebaseBranchService(r *ReconcileCodebaseBranch) (CodebaseBranchService, error) {
-	return impl.CodebaseBranchService{
-		r.client,
-	}, nil
-}
-
-func (r *ReconcileCodebaseBranch) updateStatus(instance *edpv1alpha1.CodebaseBranch) {
-	err := r.client.Status().Update(context.TODO(), instance)
-	if err != nil {
-		_ = r.client.Update(context.TODO(), instance)
-	}
 }
 
 func (r *ReconcileCodebaseBranch) getJenkinsFolder(name, namespace string) (*v1alpha1.JenkinsFolder, error) {
---
+++ pkg/controller/codebasebranch/service/codebasebranch.go
@@ -0,0 +1,136 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"github.com/epmd-edp/codebase-operator/v2/pkg/apis/edp/v1alpha1"
+	edpv1alpha1 "github.com/epmd-edp/codebase-operator/v2/pkg/apis/edp/v1alpha1"
+	"github.com/epmd-edp/codebase-operator/v2/pkg/jenkins"
+	"github.com/epmd-edp/codebase-operator/v2/pkg/model"
+	"github.com/epmd-edp/codebase-operator/v2/pkg/openshift"
+	"github.com/pkg/errors"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"time"
+)
+
+var log = logf.Log.WithName("codebase_branch_service")
+
+const jenkinsJobSuccessStatus = "blue"
+
+type CodebaseBranchService struct {
+	Cs openshift.ClientSet
+}
+
+func (s *CodebaseBranchService) TriggerReleaseJob(cb *v1alpha1.CodebaseBranch) error {
+	if cb.Status.Status != model.StatusInit {
+		log.V(2).Info("Release for codebase is not in init status. Skipped.",
+			"release", cb.Spec.BranchName, "codebase", cb.Spec.CodebaseName)
+		return nil
+	}
+
+	rLog := log.WithValues("codebase branch", cb.Name, "codebase", cb.Spec.CodebaseName)
+	rLog.V(2).Info("start triggering release job")
+
+	if err := s.setIntermediateStatus(cb, edpv1alpha1.AcceptCodebaseBranchRegistration); err != nil {
+		return err
+	}
+	jc, err := initJenkinsClient(s.Cs, cb.Namespace)
+	if err != nil {
+		if err := s.setFailStatus(cb, edpv1alpha1.JenkinsConfiguration, err.Error()); err != nil {
+			return err
+		}
+		return errors.Wrap(err, "couldn't create jenkins client")
+	}
+	rLog.V(2).Info("start creating release for codebase")
+
+	if err = jc.TriggerReleaseJob(cb.Spec.BranchName, cb.Spec.FromCommit, cb.Spec.CodebaseName); err != nil {
+		if err := s.setFailStatus(cb, edpv1alpha1.JenkinsConfiguration, err.Error()); err != nil {
+			return err
+		}
+		return err
+	}
+	rLog.Info("Release job has been triggered")
+
+	rj := fmt.Sprintf("%v/job/Create-release-%v", cb.Spec.CodebaseName, cb.Spec.CodebaseName)
+	js, err := jc.GetJobStatus(rj, 10*time.Second, 50)
+	if err != nil {
+		if err := s.setFailStatus(cb, edpv1alpha1.JenkinsConfiguration, err.Error()); err != nil {
+			return err
+		}
+		return err
+	}
+
+	if js != jenkinsJobSuccessStatus {
+		rLog.Info("failed to create release", "release job status", js)
+		return nil
+	}
+	rLog.Info("release has been created. Status: %v", model.StatusFinished)
+	return s.setSuccessStatus(cb, edpv1alpha1.JenkinsConfiguration)
+}
+
+func initJenkinsClient(cs openshift.ClientSet, namespace string) (*jenkins.JenkinsClient, error) {
+	j, err := jenkins.GetJenkins(cs.Client, namespace)
+	if err != nil {
+		return nil, err
+	}
+	jt, ju, err := jenkins.GetJenkinsCreds(*j, cs, namespace)
+	if err != nil {
+		return nil, err
+	}
+	jurl := jenkins.GetJenkinsUrl(*j, namespace)
+	jc, err := jenkins.Init(jurl, ju, jt)
+	if err != nil {
+		return nil, err
+	}
+	log.V(2).Info("jenkins client has been created", "url", jurl, "user", ju)
+	return jc, nil
+}
+
+func (s *CodebaseBranchService) setIntermediateStatus(cb *v1alpha1.CodebaseBranch, action v1alpha1.ActionType) error {
+	cb.Status = v1alpha1.CodebaseBranchStatus{
+		LastTimeUpdated: time.Now(),
+		Username:        "system",
+		Action:          action,
+		Result:          "success",
+		Value:           "inactive",
+		Status:          model.StatusInit,
+	}
+	return s.updateStatus(cb)
+}
+
+func (s *CodebaseBranchService) setFailStatus(cb *v1alpha1.CodebaseBranch, action v1alpha1.ActionType, msg string) error {
+	cb.Status = v1alpha1.CodebaseBranchStatus{
+		LastTimeUpdated: time.Now(),
+		Username:        "system",
+		Action:          action,
+		Result:          edpv1alpha1.Error,
+		DetailedMessage: msg,
+		Value:           "failed",
+		Status:          model.StatusInit,
+	}
+	return s.updateStatus(cb)
+
+}
+
+func (s *CodebaseBranchService) setSuccessStatus(cb *v1alpha1.CodebaseBranch, action v1alpha1.ActionType) error {
+	cb.Status = v1alpha1.CodebaseBranchStatus{
+		LastTimeUpdated: time.Now(),
+		Username:        "system",
+		Action:          action,
+		Result:          edpv1alpha1.Success,
+		Value:           "active",
+		Status:          model.StatusFinished,
+	}
+	return s.updateStatus(cb)
+
+}
+
+func (s *CodebaseBranchService) updateStatus(cb *v1alpha1.CodebaseBranch) error {
+	if err := s.Cs.Client.Status().Update(context.TODO(), cb); err != nil {
+		if err := s.Cs.Client.Update(context.TODO(), cb); err != nil {
+			return errors.Wrap(err, "couldn't update codebase branch status")
+		}
+	}
+	log.V(2).Info("codebase branch status has been updated", "name", cb.Name)
+	return nil
+}
