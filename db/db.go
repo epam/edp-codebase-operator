epmd-edp/codebase-operator

[MDTUDDM-673] Add K8S implementation for work with git status
Change-Id: I7244c2a5af1ec39bebc48c89cf1b4cb2535a0a51

db/db.go
@@ -13,6 +13,11 @@ var log = logf.Log.WithName("db-connector")
 
 func GetConnection() *sql.DB {
 	log.V(2).Info("start creating connection to DB")
+	dbUse := getEnvOrDefault("DB_USE", "true")
+	if dbUse != "true" {
+		log.V(2).Info("usage of database is disabled")
+		return nil
+	}
 	host := getEnvOrFatal("DB_HOST")
 	port := getEnvOrFatal("DB_PORT")
 	name := getEnvOrFatal("DB_NAME")

pkg/controller/codebase/repository/sql.go
@@ -1,0 +1,40 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const (
+	selectProjectStatusValue = "select project_status from \"%v\".codebase where name = $1 ;"
+	setProjectStatusValue    = "update \"%v\".codebase set project_status = $1 where name = $2 ;"
+)
+
+type SqlCodebaseRepository struct {
+	DB *sql.DB
+}
+
+func (r SqlCodebaseRepository) SelectProjectStatusValue(name, schema string) (*string, error) {
+	stmt, err := r.DB.Prepare(fmt.Sprintf(selectProjectStatusValue, schema))
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	var s *string
+	if err = stmt.QueryRow(name).Scan(&s); err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+func (r SqlCodebaseRepository) UpdateProjectStatusValue(status, name, schema string) error {
+	stmt, err := r.DB.Prepare(fmt.Sprintf(setProjectStatusValue, schema))
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(status, name)
+	return err
+}

epam/edp-codebase-operator

Change-Id: Ida573aaaf761f80f8b3f52d9b1bada8467072ca6

db/db.go
@@ -13,6 +13,11 @@ var log = logf.Log.WithName("db-connector")
 
 func GetConnection() *sql.DB {
 	log.V(2).Info("start creating connection to DB")
+	dbUse := getEnvOrDefault("DB_ENABLED", "true")
+	if dbUse != "true" {
+		log.V(2).Info("usage of database is disabled")
+		return nil
+	}
 	host := getEnvOrFatal("DB_HOST")
 	port := getEnvOrFatal("DB_PORT")
 	name := getEnvOrFatal("DB_NAME")

pkg/controller/codebase/repository/sql/sql.go
@@ -1,0 +1,40 @@
+package sql
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const (
+	selectProjectStatusValue = "select project_status from \"%v\".codebase where name = $1 ;"
+	setProjectStatusValue    = "update \"%v\".codebase set project_status = $1 where name = $2 ;"
+)
+
+type CodebaseRepository struct {
+	DB *sql.DB
+}
+
+func (r CodebaseRepository) SelectProjectStatusValue(name, schema string) (*string, error) {
+	stmt, err := r.DB.Prepare(fmt.Sprintf(selectProjectStatusValue, schema))
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	var s *string
+	if err = stmt.QueryRow(name).Scan(&s); err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+func (r CodebaseRepository) UpdateProjectStatusValue(status, name, schema string) error {
+	stmt, err := r.DB.Prepare(fmt.Sprintf(setProjectStatusValue, schema))
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(status, name)
+	return err
+}