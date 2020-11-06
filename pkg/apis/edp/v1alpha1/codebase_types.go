fac13626a6f8c11214dddd6e39ea5656d587ca0f
Taras Prokhorov <taras_prokhorov@epam.com>
2020-11-06 15:33:15 +0200

    [EPMDEDP-5717]: Modified Codebase API to store new value defaultBranch
    Change-Id: I1e68110f4e2ffba5d784c9d6c42ce71c86e986ea

--- pkg/apis/edp/v1alpha1/codebase_types.go
+++ pkg/apis/edp/v1alpha1/codebase_types.go
@@ -61,16 +61,17 @@ type CodebaseSpec struct {
 	TestReportFramework  *string     `json:"testReportFramework"`
 	Type                 string      `json:"type"`
 	GitServer            string      `json:"gitServer"`
-	GitUrlPath           *string     `json:"gitUrlPath"`
-	JenkinsSlave         *string     `json:"jenkinsSlave"`
-	JobProvisioning      *string     `json:"jobProvisioning"`
-	DeploymentScript     string      `json:"deploymentScript"`
-	Versioning           Versioning  `json:"versioning"`
-	JiraServer           *string     `json:"jiraServer,omitempty"`
-	CommitMessagePattern *string     `json:"commitMessagePattern"`
-	TicketNamePattern    *string     `json:"ticketNamePattern"`
-	CiTool               string      `json:"ciTool"`
-	Perf                 *Perf       `json:"perf"`
+	GitUrlPath           *string    `json:"gitUrlPath"`
+	JenkinsSlave         *string    `json:"jenkinsSlave"`
+	JobProvisioning      *string    `json:"jobProvisioning"`
+	DeploymentScript     string     `json:"deploymentScript"`
+	Versioning           Versioning `json:"versioning"`
+	JiraServer           *string    `json:"jiraServer,omitempty"`
+	CommitMessagePattern *string    `json:"commitMessagePattern"`
+	TicketNamePattern    *string    `json:"ticketNamePattern"`
+	CiTool               string     `json:"ciTool"`
+	Perf                 *Perf      `json:"perf"`
+	DefaultBranch        string     `json:"defaultBranch"`
 }
 
 // CodebaseStatus defines the observed state of Codebase
