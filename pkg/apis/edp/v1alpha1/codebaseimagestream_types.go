f851d06a3eab01054851c403566fff20fcb69ead
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2021-02-24 15:24:45 +0200

    [EPMDEDP-6114]: Extend CodebaseImageStream with codebase name field
    Change-Id: Id1a841c17202ed33f11dc45e263c29108a9fd876

--- pkg/apis/edp/v1alpha1/codebaseimagestream_types.go
+++ pkg/apis/edp/v1alpha1/codebaseimagestream_types.go
@@ -13,6 +13,7 @@ type CodebaseImageStreamSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Codebase  string `json:"codebase"`
 	ImageName string `json:"imageName"`
 	Tags      []Tag  `json:"tags, omitempty"`
 }
